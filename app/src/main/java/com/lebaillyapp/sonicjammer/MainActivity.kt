05cdce136d24baeb0fd5910c92f336797d1b23f8
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-28 22:36:36 +0200

    chore(global): Heavy push for a basic save, code will be reworked later... part 3

--- app/src/main/java/com/lebaillyapp/sonicjammer/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/sonicjammer/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.lebaillyapp.sonicjammer
 
-import android.os.Build
 import android.os.Bundle
-import android.util.MutableInt
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -33,12 +21,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.lebaillyapp.sonicjammer.composable.FiveDigitDisplay
-import com.lebaillyapp.sonicjammer.composable.SevenSegmentDisplay
-import com.lebaillyapp.sonicjammer.composable.SevenSegmentDisplayVariante
-import com.lebaillyapp.sonicjammer.composable.SingleSegmentWithGlowAndGradientFlicker
+import com.lebaillyapp.sonicjammer.composable.afficheurs.DynamikRowAfficheur
+import com.lebaillyapp.sonicjammer.composable.afficheurs.FiveDigitReflect
+import com.lebaillyapp.sonicjammer.composable.sevenSeg.SevenSegEvo
+import com.lebaillyapp.sonicjammer.composable.sevenSeg.SevenSegmentDisplayExtended
+import com.lebaillyapp.sonicjammer.config.SevenSegmentConfig
 import com.lebaillyapp.sonicjammer.ui.theme.SonicJammerTheme
 import kotlinx.coroutines.delay
 
@@ -52,10 +40,9 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             var demoValue by remember { mutableIntStateOf(0) }
-
             LaunchedEffect(Unit) {
                 while (true) {
-                    delay(100)
+                    delay(10)
                     // We'll increment demoValue up to 99999 to use all 5 digits
                     demoValue = (demoValue + 1) % 100000
                 }
@@ -71,136 +58,136 @@ class MainActivity : ComponentActivity() {
                     contentAlignment = Alignment.Center
                 ) {
 
-                    Column(modifier = Modifier.fillMaxSize()) {
+                    Spacer(modifier = Modifier.height(30.dp))
 
 
-                        // Extracting digits from demoValue
-                        val digit1 = (demoValue / 10000) % 10
-                        val digit2 = (demoValue / 1000) % 10
-                        val digit3 = (demoValue / 100) % 10
-                        val digit4 = (demoValue / 10) % 10
-                        val digit5 = demoValue % 10
+                    // general mutual config
+                    val segLgt = 15.dp
+                    val segHorLgt = 15.dp
+                    val segThk = 3.dp
+                    val bevel = 1.dp
+                    val offCol = Color(0xFF211D1D)
+                    val flickAmpl = 0.25f
+                    val flickFreq = 2f
+                    val glowRad = 20f
 
-                        Spacer(modifier = Modifier.height(140.dp))
-                        FiveDigitDisplay(
+                    //Config list
+                    val configlist = listOf(
+
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)
+                        ),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFF1DE9B6)),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFFFF9100)
+                        ),
+                        SevenSegmentConfig(segmentLength = segLgt,segmentHorizontalLength = segHorLgt, segmentThickness = segThk,bevel = bevel, offColor = offCol,flickerAmplitude = flickAmpl,flickerFrequency = flickFreq,glowRadius = glowRad,
+                            onColor = Color(0xFFFF9100)),
+
+
+                    )
+
+
+                    val configlistB = listOf(
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(0xFF00E5FF) ),
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(0xFF00E5FF) ),
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(0xFF00E5FF) ),
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(0xFF00E5FF) ),
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(0xFF00E5FF) ),
+                        SevenSegmentConfig(offColor = Color(0xFF2A1313),segmentLength = 17.5.dp,segmentHorizontalLength = 17.5.dp, segmentThickness = 4.5.dp,bevel = 2.dp,glowRadius = 70f, onColor = Color(
+                            0xFFFF1744
+                        )
+                        ),
+                    )
+
+
+                    val configlistD = iteratorConfigGenerator(
+                        sevenSegCfg = SevenSegmentConfig(
+                                digit = 1,
+                                char = null,
+                                manualSegments = null,
+                                segmentLength = 50.dp,
+                                segmentHorizontalLength = 25.dp,
+                                segmentThickness = 8.dp,
+                                bevel = 4.dp,
+                                onColor = Color(0xFFFF1744),
+                                offColor = Color(0xFF2A332F),
+                                alpha = 1f,
+                                glowRadius = 70f,
+                                flickerAmplitude = 0.05f,
+                                flickerFrequency = 1f,
+                                idleMode = false,
+                                idleSpeed = 100
+                        ),
+                        nbrDigit = 7
+                    )
+
+
+
+
+
+                    Column {
+                        DynamikRowAfficheur(configs = configlist,
+                            overrideValue = "${demoValue*139}",
+                            reversedOverride = true,
+                            showZeroWhenEmpty = true,
+                            activateReflect = false,
+                            reflectAlpha = 0.3f,
+                            reflectSpacing = 2.dp,
+                            reflectAngle = 245f,
+                            reflectGlowRadius = 10f,
+                            reflectCameraAdjustment = 6f,
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            digit1 = digit1,
-                            digit2 = digit2,
-                            digit3 = digit3,
-                            digit4 = digit4,
-                            digit5 = digit5,
-                            digit1Color = Color(0xFF00E676),
-                            digit2Color = Color(0xFF00E676),
-                            digit3Color = Color(0xFF00E676),
-                            digit4Color = Color(0xFFFFFFFF),
-                            digit5Color = Color(0xFFFFFFFF),
-                            digit1GlowRadius = 70f,
-                            digit2GlowRadius = 70f,
-                            digit3GlowRadius = 70f,
-                            digit4GlowRadius = 70f,
-                            digit5GlowRadius = 70f,
-                            digit1FlickerAmplitude = 0.05f,
-                            digit2FlickerAmplitude = 0.05f,
-                            digit3FlickerAmplitude = 0.05f,
-                            digit4FlickerAmplitude = 0.05f,
-                            digit5FlickerAmplitude = 0.05f,
-                            digit1FlickerFrequency = 1f,
-                            digit2FlickerFrequency = 1f,
-                            digit3FlickerFrequency = 1f,
-                            digit4FlickerFrequency = 1.0f,
-                            digit5FlickerFrequency = 1.0f,
-                            offColor = Color(0xFF2A332F),
-                            segmentLength = 9.dp,
-                            segmentHorizontalLength = 9.dp,
-                            segmentThickness = 3.dp,
-                            bevel = 1.5.dp,
-                            alpha = 1f,
-                            spacer = 4.dp,
+                            extraSpacingStep = 2,
+                            extraSpacing = 15.dp
+
                         )
 
-                        Spacer(modifier = Modifier.height(50.dp))
+                        Spacer(modifier = Modifier.height(80.dp))
 
-                        FiveDigitDisplay(
+
+                        DynamikRowAfficheur(configs = configlistB,
+                            overrideValue = "$demoValue",
+                            reversedOverride = true,
+                            showZeroWhenEmpty = true,
+                            activateReflect = true,
+                            reflectAlpha = 0.3f,
+                            reflectSpacing = 0.dp,
+                            reflectAngle = 245f,
+                            reflectGlowRadius = 35f,
+                            reflectCameraAdjustment = 4f,
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            digit1 = digit1,
-                            digit2 = digit2,
-                            digit3 = digit3,
-                            digit4 = digit4,
-                            digit5 = digit5,
-                            digit1Color = Color(0xFF00F7FF),
-                            digit2Color = Color(0xFF00E5FF),
-                            digit3Color = Color(0xFF00E5FF),
-                            digit4Color = Color(0xFF00E5FF),
-                            digit5Color = Color(0xFFFF1744),
-                            digit1GlowRadius = 70f,
-                            digit2GlowRadius = 70f,
-                            digit3GlowRadius = 70f,
-                            digit4GlowRadius = 70f,
-                            digit5GlowRadius = 70f,
-                            digit1FlickerAmplitude = 0.05f,
-                            digit2FlickerAmplitude = 0.15f,
-                            digit3FlickerAmplitude = 0.10f,
-                            digit4FlickerAmplitude = 0.20f,
-                            digit5FlickerAmplitude = 0.1f,
-                            digit1FlickerFrequency = 1f,
-                            digit2FlickerFrequency = 0.5f,
-                            digit3FlickerFrequency = 1f,
-                            digit4FlickerFrequency = 1.0f,
-                            digit5FlickerFrequency = 1.0f,
-                            offColor = Color(0xFF2A332F),
-                            segmentLength = 17.5.dp,
-                            segmentHorizontalLength = 17.5.dp,
-                            segmentThickness = 4.5.dp,
-                            bevel = 2.dp,
-                            alpha = 1f,
-                            spacer = 10.dp,
+
                         )
 
-                        Spacer(modifier = Modifier.height(50.dp))
+                        Spacer(modifier = Modifier.height(80.dp))
 
-                        FiveDigitDisplay(
+
+                        DynamikRowAfficheur(configs = configlistD,
+                            overrideValue = "${demoValue*39}",
+                            reversedOverride = true,
+                            showZeroWhenEmpty = true,
+                            activateReflect = false,
+                            reflectAlpha = 0.3f,
+                            reflectSpacing = 0.dp,
+                            reflectAngle = 245f,
+                            reflectGlowRadius = 35f,
+                            reflectCameraAdjustment = 4f,
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            digit1 = 2,
-                            digit2 = 4,
-                            digit3 = 6,
-                            digit4 = 3,
-                            digit5 = 9,
-                            digit1Color = Color(0xFFC6FF00),
-                            digit2Color = Color(0xFFC6FF00),
-                            digit3Color = Color(0xFFC6FF00),
-                            digit4Color = Color(0xFFC6FF00),
-                            digit5Color = Color(0xFF651FFF),
-                            digit1GlowRadius = 50f,
-                            digit2GlowRadius = 50f,
-                            digit3GlowRadius = 50f,
-                            digit4GlowRadius = 50f,
-                            digit5GlowRadius = 50f,
-                            digit1FlickerAmplitude = 0.05f,
-                            digit2FlickerAmplitude = 0.15f,
-                            digit3FlickerAmplitude = 0.10f,
-                            digit4FlickerAmplitude = 0.20f,
-                            digit5FlickerAmplitude = 0.1f,
-                            digit1FlickerFrequency = 1f,
-                            digit2FlickerFrequency = 0.5f,
-                            digit3FlickerFrequency = 1f,
-                            digit4FlickerFrequency = 1.0f,
-                            digit5FlickerFrequency = 1.0f,
-                            offColor = Color(0xFF2A332F),
-                            segmentLength = 18.dp,
-                            segmentHorizontalLength = 22.dp,
-                            segmentThickness = 4.dp,
-                            bevel = 2.dp,
-                            alpha = 1f,
-                            spacer = 6.dp,
-                        )
-
-
+                            )
 
 
 
                     }
-
-
 
 
                  }
@@ -209,3 +196,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+fun iteratorConfigGenerator(sevenSegCfg: SevenSegmentConfig,nbrDigit: Int): List<SevenSegmentConfig>{
+    val list = mutableListOf<SevenSegmentConfig>()
+    for (i in 1..nbrDigit){
+        list.add(sevenSegCfg)
+    }
+    return list
+}
