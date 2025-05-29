a0fe78ec42a24ded352c7ab552ebc1bdae75bf8f
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-29 23:25:16 +0200

    feature(config): Add a reflection configuration data object to ease the 7-segment reflection parameters.

---
+++ app/src/main/java/com/lebaillyapp/sonicjammer/config/reflectConfig.kt
@@ -0,0 +1,14 @@
+package com.lebaillyapp.sonicjammer.config
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class reflectConfig(
+    val reflectAlpha: Float = 0.3f,
+    val reflectSpacing: Dp = 0.dp,
+    val reflectAngle: Float = 245f,
+    val reflectGlowRadius: Float = 20f,
+    val reflectCameraAdjustment: Float = 6f
+
+)
